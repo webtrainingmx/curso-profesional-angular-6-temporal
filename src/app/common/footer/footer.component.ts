f05124e9e9c1d40cb8d1e9d4ac239cc2292aad0e
Alex Arriaga <alex.arriaga@base22.com>
2018-05-29 20:50:40 -0500

    Agregar nuevos estilos

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app-webtraining-footer',
     templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+    styleUrls: ['./footer.component.scss'],
+    encapsulation: ViewEncapsulation.Emulated
   }
 )
 export class FooterComponent {
